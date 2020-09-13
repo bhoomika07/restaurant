820409ea1dcac1e63de6e8da81ee7d5a759faab8
Bhoomika Valani <65896648+bhoomika07@users.noreply.github.com>
2020-09-13 19:07:25 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".btn-warning").click(function(){
+        $("#reserve").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $(".btn-success").click(function(){
+        $("#loginModal").modal('show');
+    });
+});
